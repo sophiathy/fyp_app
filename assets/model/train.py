3fa14866150888abc2d50d7274b8070c47cd54b8
Sophia Tang <sophiathy2@gmail.com>
2020-12-27 18:34:40 +0800

    27/12: - Moved model files to assets

--- assets/model/train.py
+++ assets/model/train.py
@@ -3,12 +3,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
+
 from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
+
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import LSTM, Dense, Flatten
 from tensorflow.keras.regularizers import l2
-from tensorflow.keras.optimizers import Adam
+
 from tensorflow.keras.callbacks import ModelCheckpoint
+
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -77,61 +80,66 @@ for dir_path, dir_names, filenames in os.walk(path):
             temp = pd.read_csv(path+name, header=1)
             mem = pd.concat([mem, temp], sort=False)
 
-#extract the corresponding data from dataframe
-leftPock = mem.iloc[:, np.r_[1:7, 54]]
-rightPock = mem.iloc[:, np.r_[12:18, 54]]
-wrist = mem.iloc[:, np.r_[23:29, 54]]
-upperArm = mem.iloc[:, np.r_[34:40, 54]]
-belt = mem.iloc[:, np.r_[45:51, 54]]
-# labels = mem[mem.columns[54]]
-# labels.columns = ['Activity']
+leftPock = mem[mem.columns[1:10]]
+rightPock = mem[mem.columns[15:24]]
+rightPock.columns = leftPock.columns
+train = pd.concat([leftPock, rightPock], sort=False)
+# #extract the corresponding data from dataframe
+# leftPock = mem.iloc[:, np.r_[1:7, 54]]
+# rightPock = mem.iloc[:, np.r_[12:18, 54]]
+# wrist = mem.iloc[:, np.r_[23:29, 54]]
+# upperArm = mem.iloc[:, np.r_[34:40, 54]]
+# belt = mem.iloc[:, np.r_[45:51, 54]]
+# # labels = mem[mem.columns[54]]
+# # labels.columns = ['Activity']
 
-#same column names with leftPock
-belt.columns = upperArm.columns = wrist.columns = rightPock.columns = leftPock.columns
+# #same column names with leftPock
+# belt.columns = upperArm.columns = wrist.columns = rightPock.columns = leftPock.columns
 
-#concatenate splitted data into one dataframe again
-train_data = pd.concat([leftPock, rightPock, wrist, upperArm, belt], sort=False)
+# #concatenate splitted data into one dataframe again
+# train_data = pd.concat([leftPock, rightPock, wrist, upperArm, belt], sort=False)
 
-#split: x = Accelerometer and Gyroscope data, y = Activity type
-trainX = train_data[train_data.columns[:6]]
-trainY = train_data[train_data.columns[6:7]]
+# #split: x = Accelerometer and Gyroscope data, y = Activity type
+# trainX = train_data[train_data.columns[:6]]
+# trainY = train_data[train_data.columns[6:7]]
 
-#encode labels stored in trainY
-encoder = LabelEncoder()
-trainY = encoder.fit_transform(trainY)
-trainY[:10]
+# #encode labels stored in trainY
+# encoder = LabelEncoder()
+# trainY = encoder.fit_transform(trainY)
+# trainY[:10]
 
-#80% of data used for training, 20% testing
-def data_split(x, y, train_size = 0.8):
-    s = int(train_size * len(x))
-    trainX = x[:s]
-    testX = x[s:]
-    trainY = y[:s]
-    testY = y[s:]
-    return trainX, testX, trainY, testY
+# #80% of data used for training, 20% testing
+# def data_split(x, y, train_size = 0.8):
+#     s = int(train_size * len(x))
+#     trainX = x[:s]
+#     testX = x[s:]
+#     trainY = y[:s]
+#     testY = y[s:]
+#     return trainX, testX, trainY, testY
 
-trainX, testX, trainY, testY = data_split(trainX, trainY)
+# trainX, testX, trainY, testY = data_split(trainX, trainY)
 
-#convert data into time series seq
-features = 9
-window_size = 100
+# #convert data into time series seq
+# features = 9
+# window_size = 100
 
-train = TimeseriesGenerator(trainX.to_numpy(), trainY, length=window_size, batch_size=1024)
-test = TimeseriesGenerator(testX.to_numpy(), testY, length=window_size, batch_size=1024)
+# train = TimeseriesGenerator(trainX.to_numpy(), trainY, length=window_size, batch_size=1024)
+# test = TimeseriesGenerator(testX.to_numpy(), testY, length=window_size, batch_size=1024)
 
-model = Sequential()
-model.add(LSTM(32, return_sequences=True, input_shape=(window_size, features),
-    kernel_regularizer=l2(0.000001), bias_regularizer=l2(0.000001), name='lstm_1'))
+# model = Sequential()
+# model.add(LSTM(32, return_sequences=True, input_shape=(window_size, features),
+#     kernel_regularizer=l2(0.000001), bias_regularizer=l2(0.000001), name='lstm_1'))
 
-model.add(Flatten(name='flatten'))
-model.add(Dense(64, activation='relu', kernel_regularizer=l2(0.000001), bias_regularizer=l2(0.000001), name='dense_1'))
-model.add(Dense(len(np.unique(trainY)), activation='softmax', kernel_regularizer=l2(0.000001), bias_regularizer=l2(0.000001), name='output'))
+# model.add(Flatten(name='flatten'))
+# model.add(Dense(64, activation='relu', kernel_regularizer=l2(0.000001), bias_regularizer=l2(0.000001), name='dense_1'))
+# model.add(Dense(len(np.unique(trainY)), activation='softmax', kernel_regularizer=l2(0.000001), bias_regularizer=l2(0.000001), name='output'))
 
-model.compile(loss='sparse_categorial_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+# #compile the model
+# model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-callB = [ModelCheckpoint('model.h5', save_best_only = True, save_weights_only=False, verbose=1)]
+# callBacks = [ModelCheckpoint('model.h5', save_best_only = True, save_weights_only=False, verbose=1)]
 
-#training
-# train = np.asarray(train)
-# test = np.asarray(test)
-history = model.fit_generator(train, epochs=5, validation_data=test, callbacks=callB)
+# #training
+# history = model.fit(train, epochs=5, validation_data=test, callbacks=callBacks)
+# # model = load_model('model.h5')
+# # model.summary()

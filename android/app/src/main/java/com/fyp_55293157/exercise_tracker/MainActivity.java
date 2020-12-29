50a6100d24b1a0bebe0c2e8c0d1c8ae5a23bd9a5
Sophia Tang <sophiathy2@gmail.com>
2020-12-29 22:03:04 +0800

    29/12: - Converted Kotlin MainActivity into Java version

---
+++ android/app/src/main/java/com/fyp_55293157/exercise_tracker/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fyp_55293157.exercise_tracker;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/fyp_55293157/exercise_tracker/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.fyp_55293157.exercise_tracker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
